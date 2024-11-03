5f29b08180c13650da6b4abcb935c78e211df9f7
unxcii <84338880+mewjik01@users.noreply.github.com>
2024-11-03 10:40:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from "react"
+import "./App.css"
+function App() {
+  let [color, setColor]= useState("red")
+  
+return(
+      <div className="btn" style={{backgroundColor: color}}>
+     <button className="red" onClick={()=> setColor("red")}>RED</button>
+     <button className="yellow" onClick={()=> setColor("yellow")}>YELLOW</button>
+     <button className="green" onClick={()=> setColor("green")}>GREEN</button>
+     <button className="blue" onClick={()=> setColor("blue")}>BLUE</button>
+     <button className="black" onClick={()=> setColor("black")}>BLACK</button>
+     <button className="orange" onClick={()=> setColor("orange")}>ORANGE</button>
+      </div>
+  )
+  
+}
+export default App
